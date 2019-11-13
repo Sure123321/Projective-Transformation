4307ce05f3d9201a15ae9163c66c4a1cc6572092
Quantum Cat <veronika.netch@gmail.com>
2019-11-13 12:54:32 +0200

    Wrong version

--- main.cpp
+++ main.cpp
@@ -18,88 +18,81 @@ public:
     }
 };
 
-int side(Point start, Point end, Point point) {
-    return (end.x - start.x)*(point.y - start.y) - (end.y - start.y)*(point.x - start.x);
+void solve_matrix(int rank, double A[8][9])
+{
+    const int nrows = rank;
+    const int ncols = rank+1;
+
+    int lead = 0;
+
+    while (lead < nrows) {
+        double d, m;
+
+        for (int r = 0; r < nrows; r++) {
+            d = A[lead][lead];
+            m = A[r][lead] / A[lead][lead];
+
+            for (int c = 0; c < ncols; c++) {
+                if (r == lead) {
+                    A[r][c] /= d;
+                } else {
+                    A[r][c] -= A[lead][c] * m;
+                }
+            }
+        }
+
+        lead++;
+    }
 }
 
-vector<Point> order_points (vector<Point> points){
+Point multiply(double matrix[3][3], int x, int y) {
+    double x1 = matrix[0][0]*x + matrix[1][0]*y + matrix[2][0];
+    double y1 = matrix[0][1]*x + matrix[1][1]*y + matrix[2][1];
+    double z1 = matrix[0][2]*x + matrix[1][2]*y + matrix[2][2];
+    return Point(int(x1/z1), int(y1/z1));
+}
 
-    Point corner = points[0];
-    Point p1 = points[1];
-    Point p2 = points[2];
-    Point p3 = points[3];
-
-    Point opposite, leftside, rightside;
-    if (side(corner, p1, p2) * side(corner, p1, p3) < 0) {
-        opposite = p1;
-        if (side(corner, p1, p2) < 0) {
-            leftside = p2;
-            rightside = p3;
-        }
-        else {
-            leftside = p3;
-            rightside = p2;
-        }
-    }
-    else if (side(corner, p2, p1) * side(corner, p2, p3) < 0) {
-        opposite = p2;
-        if (side(corner, p2, p1) < 0) {
-            leftside = p1;
-            rightside = p3;
-        }
-        else {
-            leftside = p3;
-            rightside = p1;
-        }
-    }
-    else if (side(corner, p3, p1) * side(corner, p3, p2) < 0) {
-        opposite = p3;
-        if (side(corner, p3, p1) < 0) {
-            leftside = p1;
-            rightside = p2;
-        }
-        else {
-            leftside = p2;
-            rightside = p1;
-        }
-    }
-    else {
-        throw("Computation error: right points order not found");
-    }
-
-    vector<Point> answer;
-    answer.push_back(corner);
-    answer.push_back(leftside);
-    answer.push_back(opposite);
-    answer.push_back(rightside);
-    return answer;
+bool in_bounds(int x, int y, int width, int height) {
+    return x >= 0 && x < width && y >= 0 && y < height;
 }
 
 int main()
 {
     const int n = 4;
+    const int max_width = 1000;
     ifstream in;
+    ofstream out;
     in.open("input.txt");
+    out.open("output.txt");
+
+    int w, h;
+    in >> w >> h;
+    char image[max_width][max_width];
+
     vector <Point> source;
     for (int i = 0; i < n; i++) {
         int x, y;
         in >> x >> y;
         source.push_back(Point(x, y));
     }
-    //source = order_points(source);
-    // Suppose that the first point is the upper-right corner.
-    // We need to order the remaining three so the go in the clockwise order.
-    // Otherwise we can flip or distort the image.
-    // There is still ambiguity, but nevermind that.
 
     vector <Point> destination;
-    for (int i = 0; i < n; i++) {
-        int x, y;
-        in >> x >> y;
-        destination.push_back(Point(x, y));
+    int a, b;
+    in >> a >> b; //rectangle sides
+    destination.push_back(Point(0, 0));
+    destination.push_back(Point(0, b));
+    destination.push_back(Point(a, 0));
+    destination.push_back(Point(a, b));
+
+    for (int y = 0; y < h; y++) {
+        for(int x = 0; x < w; x++) {
+            in.get(image[y][x]);
+        }
+        in.get();
     }
 
-    int matrix[2*n][2*n+1];
+
+    double matrix[2*n][2*n+1];
     for (int i = 0; i < 2*n; i++) {
         for (int j = 0; j < 2*n; j++) {
             matrix[i][j] = 0;
@@ -125,8 +118,34 @@ int main()
 
     }
 
-    //solve the matrix
-    for
+    solve_matrix(2*n+1, matrix);
+
+    double transform_matrix[3][3];
+    for (int i = 0; i < 8; i++) {
+        transform_matrix[i/3][i%3] = matrix[i][8];
+    }
+    transform_matrix[2][2] = 1;
+
+    char rectangle[max_width][max_width];
+
+    for (int y = 0; y < h; y++) {
+        for(int x = 0; x < w; x++) {
+            Point dest = multiply(transform_matrix, x, y);
+            if (in_bounds(dest.x, dest.y, a, b)) {
+                rectangle[dest.x][dest.y] = image[x][y];
+            }
+        }
+    }
+
+    for (int y = 0; y < b; y++) {
+        for (int x = 0; x < a; x++) {
+            out << rectangle[y][x];
+        }
+        out << endl;
+    }
+
+    in.close();
+    out.close();
 
     return 0;
 }
